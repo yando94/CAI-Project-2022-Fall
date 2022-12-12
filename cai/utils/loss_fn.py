6ae5f01bbb1d93d4e9887b421d6b86ad233acf50
Yu Ando <yuando@gmail.com>
2022-12-13 00:12:03 +0900

    Typo y -> labels

--- cai/utils/loss_fn.py
+++ cai/utils/loss_fn.py
@@ -12,13 +12,13 @@ class SmoothMacroF1Loss(nn.Module):
     def forward(self, logits_, labels):
         y_hat = self.sigmoid(logits_)
 
-        tp2 = torch.sum(y_hat * y, dim=0) * 2
-        fp = torch.sum(y_hat * (1-y), dim=0)
-        fn = torch.sum((1-y_hat) * y, dim=0)
+        tp2 = torch.sum(y_hat * labels, dim=0) * 2
+        fp = torch.sum(y_hat * (1-labels), dim=0)
+        fn = torch.sum((1-y_hat) * labels, dim=0)
 
         f1 = tp2 / (tp2 + fp + fn + torch.finfo(torch.float32).eps)
         loss = 1 - f1
         loss = loss.mean()
-        
+
         return loss
 
